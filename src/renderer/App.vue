51fbbea62be1a9890735fe24c1e6d9d4693cdbae
libin.zhou <binteam2018@163.com>
2018-12-20 14:26:08 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'prize-draw'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
